5eea6f91638d7dafbaa5668330eb012f2a00367d
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-04-30 11:52:19 +0200

    added padding between GUI.Spells

--- src/lib/game/gui/spellbook.js
+++ src/lib/game/gui/spellbook.js
@@ -5,15 +5,18 @@ ig.module(
 	'impact.impact',
 	'impact.font',
 
-	'game.entities.syllable'
+	'game.entities.spell'
 )
 .defines(function(){
 
 GUI.SpellBook = ig.Class.extend({
+    paddingBetweenSpells: 4,
 	init: function() {
-        game.players[0].mages[0].syllablePool.syllables.forEach(function(syllable, index) {
-            GUI.game.spawnEntity(EntitySyllable, 500 + 32 * index, 110, { model: syllable });
-        });
+	    var spellPadding = 0;
+        game.players[0].mages[0].spellBook.getSpells().forEach(function(spell, index) {
+            var entitySpell = GUI.game.spawnEntity(EntitySpell, 2, 2 + spellPadding, { model: spell });
+            spellPadding += entitySpell.size.y + this.paddingBetweenSpells;
+        }, this);
 	},
 
 	update: function() {}
