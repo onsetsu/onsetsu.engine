bead80f53dddeea07f4fa2dc9d6de2ad1f18da31
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-05-05 17:58:20 +0200

    spawn 10 TimelineSlots

--- src/lib/game/gui/timeline.js
+++ src/lib/game/gui/timeline.js
@@ -5,18 +5,24 @@ ig.module(
 	'impact.impact',
 	'impact.font',
 
-	'game.entities.spell'
+	'game.entities.timelineslot',
+	'game.entities.action'
 )
 .defines(function(){
 
 GUI.Timeline = ig.Class.extend({
     paddingBetweenActions: 2,
 	init: function() {
-        // TODO
-	    var spellPadding = 0;
-        game.players[0].mages[0].spellBook.getSpells().forEach(function(spell, index) {
-            var entitySpell = GUI.game.spawnEntity(EntitySpell, 2, 2 + spellPadding, { model: spell });
-            spellPadding += entitySpell.size.y + this.paddingBetweenSpells;
+        var position = { x: 500, y: 500 };
+
+        _.range(10).forEach(function(delay) {
+            var model = game.timeline.getSlotAt(delay);
+            return GUI.game.spawnEntity(
+                EntityTimelineSlot,
+                position.x + delay * (EntityTimelineSlot.prototype.size.x + + this.paddingBetweenActions),
+                position.y,
+                { model: model }
+            );
         }, this);
 	},
 
