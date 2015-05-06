0da6fd463beda749d06c9beee01b5f609eca69b3
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-05-06 15:02:39 +0200

    spawn EntityActions and listen for newly created Actions

--- src/lib/game/gui/timeline.js
+++ src/lib/game/gui/timeline.js
@@ -11,6 +11,7 @@ ig.module(
 .defines(function(){
 
 GUI.Timeline = ig.Class.extend({
+    paddingBetweenTimelineSlots: 4,
     paddingBetweenActions: 2,
 	init: function() {
         var position = { x: 500, y: 500 };
@@ -19,11 +20,34 @@ GUI.Timeline = ig.Class.extend({
             var model = game.timeline.getSlotAt(delay);
             return GUI.game.spawnEntity(
                 EntityTimelineSlot,
-                position.x + delay * (EntityTimelineSlot.prototype.size.x + + this.paddingBetweenActions),
+                position.x + delay * (EntityTimelineSlot.prototype.size.x + + this.paddingBetweenTimelineSlots),
                 position.y,
                 { model: model }
             );
         }, this);
+
+        var spawnAction = (function(action) {
+            var timelineSlot = game.timeline.getTimelineSlotFor(action);
+            var indexInTimelineSlot = timelineSlot.actions.indexOf(action);
+            return GUI.game.spawnEntity(
+                EntityAction,
+                position.x
+                    + timelineSlot.delay * (EntityTimelineSlot.prototype.size.x + + this.paddingBetweenTimelineSlots)
+                    + (EntityTimelineSlot.prototype.size.x - EntityAction.prototype.size.x) / 2,
+                position.y
+                    - indexInTimelineSlot * (EntityAction.prototype.size.y + this.paddingBetweenActions)
+                    + EntityTimelineSlot.prototype.size.y - EntityAction.prototype.size.y,
+                { model: action }
+            );
+        }).bind(this);
+
+        game.timeline.actions.forEach(spawnAction, this);
+
+        game.timeline.addAction = _.wrap(game.timeline.addAction.bind(game.timeline), function(original, action) {
+            var returnValue = original(action);
+            spawnAction(action);
+            return returnValue;
+        });
 	},
 
 	update: function() {}
