db1c276478c9f1bae63a56b6016f0cec58c20e70
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-05-12 17:29:43 +0200

    added clickable debug entity

--- src/lib/game/game.js
+++ src/lib/game/game.js
@@ -57,6 +57,54 @@ GUI.Game = ig.Game.extend({
         this.syllableBoard = new GUI.SyllableBoard();
         this.timeline = new GUI.Timeline();
         this.battlefield = new GUI.Battlefield();
+
+        EntityDebug = ig.Entity.extend({
+        	size: {x:64, y:32},
+        	animSheet: new ig.AnimationSheet('media/debug.png', 64, 32),
+        	init: function(x, y, settings) {
+        		this.parent(x, y, settings);
+
+        		this.addAnim('visible', 1, [0], true);
+                this.applySettings(settings);
+        	},
+        	applySettings: function(settings) {
+                this.settings = settings;
+        	},
+        	update: function() {
+        		this.parent();
+
+                if(this.settings.onclick && ig.input.pressed('leftclick') && GUI.game.hovered(this)) {
+                    console.log(this.settings.label);
+                    this.settings.onclick();
+                }
+        	},
+        	draw: function() {
+        		this.parent();
+
+                if(this.settings.label) {
+                    var label = this.settings.label,
+                        x = this.pos.x + this.animSheet.width / 2,
+                        y = this.pos.y + (this.animSheet.height - GUI.Font.heightForString(label)) / 2;
+                    GUI.Font.draw(label, x, y, ig.Font.ALIGN.CENTER);
+                }
+        	}
+        });
+        this.spawnEntity(EntityDebug, 250, 50, {
+            label: 'Add Player',
+            onclick: function() {
+                var player = new Player(),
+                    mage = new Mage(
+                        player,
+                        15,
+                        5,
+                        new SyllableBoard({ x: 6, y: 6 }),
+                        createTestSpellbook(),
+                        createStandardSyllablePool()
+                    );
+                game.addPlayer(player);
+                game.battlefield.addMage(mage);
+            }
+        });
 	},
 
 	update: function() {
