4ee5c72b7f82a46939fe7e96334c0d6e9888fd97
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-05-31 12:20:53 +0200

    added datGui to start the game

--- src/main.js
+++ src/main.js
@@ -2,6 +2,31 @@
 // Main
 // --------------------------------------------------------------------------------
 
-game = new Game();
-configureGameForTwoPlayers();
-var engine = new Engine();
+var game, engine, GUI;
+
+window.onload = function() {
+  var temp = {
+    main: function() {
+      game = new Game();
+      configureGameForTwoPlayers();
+      engine = new Engine();
+
+      GUI = {};
+
+      ig.module(
+      	'???'
+      )
+      .requires(
+      	'game.game'
+      )
+      .defines(function(){
+
+        ig.main( '#canvas', GUI.Game, 60, 1138, 640, 1 );
+
+      });
+    }
+  }
+
+  datGui = new dat.GUI();
+  datGui.add(temp, 'main');
+}
