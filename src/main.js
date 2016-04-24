5e558c3557dc6689cc1f94c9635ffdee02c02b67
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2016-04-24 11:28:25 +0200

    extracted Game from main

--- src/main.js
+++ src/main.js
@@ -1,30 +1,13 @@
-// --------------------------------------------------------------------------------
-// Main
-// --------------------------------------------------------------------------------
-
 import {} from 'lib/networking.js';
 import { configureGameForTwoPlayers } from 'lib/setup/setup.js';
 import { Game } from 'lib/engine/enginegame.js';
 
-window.game = undefined;
-window.env = {};
-window.GUI = {};
-window.isHost = undefined;
+var runMain = function(host) {
+  game = new Game();
+  configureGameForTwoPlayers();
+  isHost = host;
 
-ig.module(
-  'init.game'
-)
-.requires(
-  'game.game'
-)
-.defines(function(){
-  var runMain = function(host) {
-    game = new Game();
-    configureGameForTwoPlayers();
-    isHost = host;
+  ig.main( '#canvas', GUI.Game, 60, 1138, 640, 1 );
+};
 
-    ig.main( '#canvas', GUI.Game, 60, 1138, 640, 1 );
-  };
-
-  Networking.setup(runMain);
-});
+Networking.setup(runMain);
